082adc0ef41eaefe1f4dc78ca1d77f5803da907a
Salil Chandra <salil.chandra@ibm.com>
2020-07-22 13:43:57 -0400

    Test removing app deploy args

--- src/main/java/com/acmeair/restclient/CustomerClient.java
+++ src/main/java/com/acmeair/restclient/CustomerClient.java
@@ -29,7 +29,7 @@ import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient(configKey = "customerClient")
+@RegisterRestClient(configKey="customerClient")
 @Path("/")
 public interface CustomerClient {
 
