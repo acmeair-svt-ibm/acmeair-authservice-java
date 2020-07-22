e1493eebb3e97be8901976fc6dd7511deced6b78
Tanya-Chu <tanya.chu@ibm.com>
2020-07-22 11:40:00 -0400

    Add var to server.xml, reset config, remove mount

--- src/main/java/com/acmeair/restclient/CustomerClient.java
+++ src/main/java/com/acmeair/restclient/CustomerClient.java
@@ -29,7 +29,7 @@ import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient(baseUri = "http://stackaa-customer.acmeair.svc.cluster.local:9080/customer")
+@RegisterRestClient(configKey = "customerClient")
 @Path("/")
 public interface CustomerClient {
 
